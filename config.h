d51183e87db4c003188441f7be9da8d22a220fbb
testinfd41-art <testinfd41@gmail.com>
2025-11-05 08:26:13 +0530

    first commit

---
+++ config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// String used to delimit block outputs in the status.
+#define DELIMITER "  "
+
+// Maximum number of Unicode characters that a block can output.
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+// Control whether blocks are clickable.
+#define CLICKABLE_BLOCKS 1
+
+// Control whether a leading delimiter should be prepended to the status.
+#define LEADING_DELIMITER 0
+
+// Control whether a trailing delimiter should be appended to the status.
+#define TRAILING_DELIMITER 0
+
+// Define blocks for the status feed as X(icon, cmd, interval, signal).
+#define BLOCKS(X)             \
+    X("", "systemstatus", 1, 1)  \
+    X("", "diskspace", 1, 2)  \
+    X("", "updates", 86400, 3)  \
+    X("", "bt", 1, 4)  \
+    X("", "audio", 1, 5)   \
+    X("󰃠", "brightness", 1, 6) \
+    X("", "internet", 5, 7) \
+    X("", "battery", 1, 8) \
+    X("󰃰 ", "TZ=Asia/Kolkata date '+%b %d - %I:%M%p'",      60,      9) \
+    X("", "sb-volume", 0, 10) 
+
+#endif  // CONFIG_H
